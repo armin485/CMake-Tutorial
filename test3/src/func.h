4c2d92b73fc40e8e1561cd15f72f46dd514b97bb
Armin Ghani <ghani.armin@gmail.com>
2019-05-23 12:44:40 +0430

    test3 added

---
+++ test3/src/func.h
@@ -0,0 +1,3 @@
+
+int FuncAdd(int a, int b);
+
---
+++ test3/src/main.cc
@@ -0,0 +1,12 @@
+
+#include <iostream>
+#include "func.h"
+
+using namespace std;
+
+int main() {
+    cout << FuncAdd(30, 60) << endl;
+    return 0;
+}
+
+
